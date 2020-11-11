a584d60ec33a0ea20d22a1a22a3ca31245ab65fb
Shuming <geshuming2@gmail.com>
2020-11-12 07:54:49 +0800

    Initialize basic express app

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+let express = require('express')
+
+let app = express();
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+     console.log("Running RestHub on port " + port);
+});
